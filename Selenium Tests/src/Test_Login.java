450e17d487faa1d5a92e94c2d816d9b61348fbda
Saleh Sami <salehsami017@gmail.com>
2023-06-09 15:49:36 +0500

    working with error handling

--- Selenium Tests/src/Test_Login.java
+++ Selenium Tests/src/Test_Login.java
@@ -9,13 +9,21 @@ public class Test_Login {
                 "E:/uni/7 sem/DevOps/selenium/work/Assignment-3 devops/Selenium Tests/src/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://localhost:3000/Login");
-        driver.findElement(By.name("email")).sendKeys("qasimMalik123@gmail.com");
-        driver.findElement(By.id("password")).sendKeys("qasim123");
-        WebElement button = driver.findElement(By.tagName("button"));
-        Thread.sleep(3000);
-        button.click();
-        Thread.sleep(5000);
-        driver.quit();
+        String pagesource = driver.getPageSource();
+        boolean exists = pagesource.toLowerCase().contains(("Please Create an Account").toLowerCase());
+        if (exists) {
+            System.out.print("Reached Login page");
+            driver.findElement(By.name("email")).sendKeys("qasimMalik123@gmail.com");
+            driver.findElement(By.id("password")).sendKeys("qasim123");
+            WebElement button = driver.findElement(By.tagName("button"));
+            Thread.sleep(3000);
+            button.click();
+            Thread.sleep(5000);
+            driver.quit();
+        } else {
+            System.out.println("Couldn't reach to the Login page");
+        }
+
     }
 
 }
